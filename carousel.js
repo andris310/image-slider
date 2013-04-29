7de7396b895114bcc3b587e05bda58b33611d3b2
andris310 <andris310@gmail.com>
2013-04-28 17:05:24 -0700

    Added fadeIn function

--- carousel.js
+++ carousel.js
@@ -2,23 +2,23 @@ var image = document.getElementById('image');
 var imgList = document.getElementsByTagName('img');
 var imgArray = [];
 var tracker;
+var timer;
 
 for (i = 0; i < imgList.length; i+=1) {
 	imgArray.push(imgList[i].src);
 }
 
-// image.style.opacity = 1;
+function fadeIn () {
+	if (image.style.opacity >= 1) {
+		return;
+	}
+	image.style.opacity = parseFloat(image.style.opacity, 10) + 0.01;
+}
 
-// function fadeOut () {
-// 	if (image.style.opacity >= 0) {
-// 		return;
-// 	}
-// 	image.style.opacity -= 0.1;
-// }
-
-// var timer = setInterval(fadeOut, 10);
+// var timer = setInterval(fadeIn, 10);
   
 function next() {
+	image.style.opacity = 0;
 	for( i = 0; i < imgArray.length; i += 1) {
 		if(image.src === imgArray[i]) {
 			if (image.src === imgArray[imgArray.length - 1]) {
@@ -28,7 +28,6 @@ function next() {
 			}
 		}
 	}
-	
 	return (image.src = tracker);
 }
 
@@ -45,8 +44,15 @@ function previous() {
 	return (image.src = tracker);
 }
 
+
 var nextButton = document.getElementById('next');
 var previousButton = document.getElementById('previous');
 
-nextButton.onclick = next;
+// nextButton.onclick = next;
+nextButton.onclick = function() {
+	clearInterval(timer);
+	timer = setInterval(fadeIn, 10);
+	next(); 
+}
+
 previousButton.onclick = previous;
